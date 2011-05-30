515e63ef1cc79265f93dea5efaecdf72315d6663
Tom Burger Locally <tomas@burger.cz>
2011-05-31 00:27:56 +0200

    Pripravit na prvni publikovani

--- src/cz/burger/android/phonegap/mobileprsi/Main.java
+++ src/cz/burger/android/phonegap/mobileprsi/Main.java
@@ -1,13 +1,25 @@
 package cz.burger.android.phonegap.mobileprsi;
 
-import android.app.Activity;
 import android.os.Bundle;
 
-public class Main extends Activity {
-    /** Called when the activity is first created. */
+import com.google.android.apps.analytics.GoogleAnalyticsTracker;
+import com.phonegap.*;
+
+public class Main extends DroidGap {
+
+	// Google Analytics Code: UA-23677989-1
+	GoogleAnalyticsTracker tracker;
+	
+	/** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        
+        tracker = GoogleAnalyticsTracker.getInstance();
+        tracker.start("UA-23677989-1", this);
+        tracker.trackPageView("/App-Started");
+        tracker.dispatch();
+        
+        super.loadUrl("file:///android_asset/www/index.html");
     }
 }
